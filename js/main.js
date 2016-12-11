4f227ee6b0c82a6f9e870b9a53bb874492853298
Holly <hnguyen0711@gmail.com>
2016-12-10 22:37:27 -0800

    added render function

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 //Create the canvas
-var canvas = document.getElementById('board'); //access the rendering context and draw on it
-var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
+ var canvas = document.getElementById('canvas'); //access the rendering context and draw on it
+ var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
+  
+ //Create sprites
+ var player = new Image();
+ player.src = "http://i.imgur.com/aevR7XM.png";
+  	
+ var enemy = new Image();
+ enemy.src = "http://i.imgur.com/QHVh32l.png";
+  
+ var enemyBeam = new Image();
+ enemyBeam.src = "http://i.imgur.com/DukiQLC.png";
+  
+ var playerBeam = new Image();
+ playerBeam.src = "http://i.imgur.com/dMUuR5f.png";
 
-//Create sprites
-var player = new Image();
-player.src = "http://i.imgur.com/aevR7XM.png";
-	
-var enemy = new Image();
-enemy.src = "http://i.imgur.com/QHVh32l.png";
+ var score = 0;
+  	
+ var render = function() {
+ 	ctx.drawImage(player, 40,40);
+ 	ctx.drawImage(enemy, 150,150);
+ 	ctx.drawImage(enemyBeam, 200, 200);
+ 	ctx.drawImage(playerBeam, 300, 300);
+ 
+ 	ctx.font = "16px Press Start K";
+ 	ctx.fillStyle = "yellow";
+ 	ctx.fillText("Score "+score, 10, 22);
+ };
 
-var enemyBeam = new Image();
-enemyBeam.src = "http://i.imgur.com/DukiQLC.png";
+ var main = function() {
+ 	render();
+ };
 
-var playerBeam = new Image();
-playerBeam.src = "http://i.imgur.com/dMUuR5f.png";
-	
-function drawImage() {
-	ctx.drawImage(player, 40,40);
-	ctx.drawImage(enemy, 150,150);
-	ctx.drawImage(enemyBeam, 200, 200);
-	ctx.drawImage(playerBeam, 300, 300);
-}
-
-drawImage();
-
-//Create score
-var score = 0;
-
-function drawScore() {
-	ctx.font = "16px Press Start K";
-	ctx.fillStyle = "yellow";
-	ctx.fillText("Score "+score, 10, 22);
-}
-	
-drawScore();
-	
+ main();
+ 
+  
+ 
+ 
+ 
