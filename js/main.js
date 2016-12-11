4f6861d439b997fd44dc94862eb7da315d85021f
Holly <hnguyen0711@gmail.com>
2016-12-10 19:15:14 -0800

    created canvas, sprites, score

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
+//Create the canvas
+var canvas = document.getElementById('board'); //access the rendering context and draw on it
+var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
 
-	var canvas = document.getElementById('board'); //access the rendering context and draw on it
-	var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
+//Create sprites
+var player = new Image();
+player.src = "http://i.imgur.com/aevR7XM.png";
 	
-	var img = new Image();
-	img.src = "http://i.imgur.com/aevR7XM.png";
+var enemy = new Image();
+enemy.src = "http://i.imgur.com/QHVh32l.png";
+
+var enemyBeam = new Image();
+enemyBeam.src = "http://i.imgur.com/DukiQLC.png";
+
+var playerBeam = new Image();
+playerBeam.src = "http://i.imgur.com/dMUuR5f.png";
 	
-	var imgEnemy = new Image();
-	imgEnemy.src = "http://i.imgur.com/QHVh32l.png";
-	img.onload = function() {
-		ctx.drawImage(img, 40,40);
-		ctx.drawImage(imgEnemy, 150,150);
-	};
+function drawImage() {
+	ctx.drawImage(player, 40,40);
+	ctx.drawImage(enemy, 150,150);
+	ctx.drawImage(enemyBeam, 200, 200);
+	ctx.drawImage(playerBeam, 300, 300);
+}
 
+drawImage();
 
+//Create score
+var score = 0;
+
+function drawScore() {
+	ctx.font = "16px Press Start K";
+	ctx.fillStyle = "yellow";
+	ctx.fillText("Score "+score, 10, 22);
+}
 	
-
+drawScore();
 	
