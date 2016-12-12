bff1208321a9dd0ea6aad19d0090658cd78eb99d
Holly <hnguyen0711@gmail.com>
2016-12-12 11:15:09 -0800

    fixing object constructor functions

--- js/main.js
+++ js/main.js
@@ -1,38 +1,57 @@
-//Create the canvas
- var canvas = document.getElementById('canvas'); //access the rendering context and draw on it
- var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
+//Link canvas to html
+var canvas = document.getElementById('canvas'); //access the rendering context and draw on it
+var ctx = canvas.getContext('2d'); //method to obtain rendering context and drawing functions
 
- var w = window.innerWidth;
- var h = window.innerHeight;
+//Set canvas size to window width and height
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 canvas.height = h;
 canvas.width = w;
 
- //Load sprite images
- var player = new Image();
- player.src = "http://i.imgur.com/aevR7XM.png";
+//Load sprite images
+//function Image(src) {
+//	this.src = src;
+//}
+//var player = new Image("http://i.imgur.com/aevR7XM.png");
+
+var player = new Image();
+player.src = "http://i.imgur.com/aevR7XM.png";
   	
- var enemy = new Image();
- enemy.src = "http://i.imgur.com/QHVh32l.png";
+var enemy = new Image();
+enemy.src = "http://i.imgur.com/QHVh32l.png";
   
- var enemyBeam = new Image();
- enemyBeam.src = "http://i.imgur.com/DukiQLC.png";
+var enemyBeam = new Image();
+enemyBeam.src = "http://i.imgur.com/DukiQLC.png";
   
- var playerBeam = new Image();
- playerBeam.src = "http://i.imgur.com/dMUuR5f.png";
+var playerBeam = new Image();
+playerBeam.src = "http://i.imgur.com/dMUuR5f.png";
 
 //Create score to start at 0
- var score = 0;	
+var score = 0;	
 
  //Add event listeners when keys pressed to move sprites
-var keys = [];
-document.body.addEventListner('keydown', function(evt) {
-	keys[evt.keyCode] = true;
-});
+//var keys = [];//
 
-document.body.addEventListner('keyup', function(evt) {
-	keys[evt.keyCode] = false;
-});
+//document.body.addEventListner('keydown', function(evt) {
+//	keys[evt.keyCode] = true;
+//});//
+
+//document.body.addEventListner('keyup', function(evt) {
+//	keys[evt.keyCode] = false;
+//});
+
+//Create player
+//function player() {
+//	this.speed = 10;
+//	this.friction = 0.7;
+//	this.vel_x = this.vel_y = 0;
+//}
+
+//Create load method to render player
+//player.prototype.load = function() {
+//	ctx.drawImage(player, 500,520);
+//};
 
 //Draw everything 	
  function render() {
@@ -43,6 +62,7 @@ document.body.addEventListner('keyup', function(evt) {
  	ctx.font = "16px Press Start K"; //renders score
  	ctx.fillStyle = "yellow";
  	ctx.fillText("Score " +score, 10, 22);
+ 	//player.load();
  }
 
  function init() {
